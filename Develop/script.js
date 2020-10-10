7ea4d25630a5e39a785b38220dbfa3c953489449
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-10 16:20:59 -0400

    Updated arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,8 @@
 // Character Arrays:
-let specCharArr = [ !, #, $, %, &, (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, {, |, }, ~,];
+let specCharArr = [ !, #, $, %, &, (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, {, |, }, ~];
 let numArr = [0,1,2,3,4,5,6,7,8,9]
-let lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
-
-
+let lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
 // Assignment Code
