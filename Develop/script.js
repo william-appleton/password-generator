84e4887a8629550a06c4e2dd49616e26626998b0
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-10 16:59:59 -0400

    added 'return' to functions

--- Develop/script.js
+++ Develop/script.js
@@ -11,20 +11,19 @@ generateBtn.addEventListener("click", userPrompts);
 
 // Prompts for user choices:
 function userPrompts () {
-  let specCharConf = confirm("Include special characters? (Hit 'ok' to include, 'cancel' to exclude")
+  let specCharConf = confirm("Include special characters? (Hit 'ok' to include, 'cancel' to exclude)")
   let numConf = confirm("Include numbers?")
   let upperConf = confirm("Include lower case letters?")
   let lowerConf = confirm("Include upper case letters?")
-    if (specCharConf === false && numConf === false && upperConf === false && lowerConf === false)
+    if (specCharConf === false && numConf === false && upperConf === false && lowerConf === false) {
       alert("Please choose at least one character type to include!")
-
-
+      return;
+    }
   let charCount = prompt("How many characters? Must be between 8 and 128");
     if (charCount === null || charCount === "" || charCount < 8 || charCount > 128 || isNaN(charCount) === true) {
       alert("Please enter a valid length/ enter a number!");
+      return;
     } 
-
-
 }
 
 
