19f2ae728abd3492722ca458455e708a3902c544
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-10 16:54:05 -0400

    Added alert to character type confirm windows

--- Develop/script.js
+++ Develop/script.js
@@ -4,20 +4,37 @@ let numArr = [0,1,2,3,4,5,6,7,8,9]
 let lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
-// Assignment Code
+// Button selection and click event listener:
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", userPrompts);
 
-// Write password to the #password input
-function writePassword() {
-  confirm("Include special characters?")
-  confirm("Include numbers?")
-  confirm("Include upper case letters?")
-  confirm("Include lower case letters?")
+
+// Prompts for user choices:
+function userPrompts () {
+  let specCharConf = confirm("Include special characters? (Hit 'ok' to include, 'cancel' to exclude")
+  let numConf = confirm("Include numbers?")
+  let upperConf = confirm("Include lower case letters?")
+  let lowerConf = confirm("Include upper case letters?")
+    if (specCharConf === false && numConf === false && upperConf === false && lowerConf === false)
+      alert("Please choose at least one character type to include!")
+
+
   let charCount = prompt("How many characters? Must be between 8 and 128");
     if (charCount === null || charCount === "" || charCount < 8 || charCount > 128 || isNaN(charCount) === true) {
       alert("Please enter a valid length/ enter a number!");
     } 
+
+
+}
+
+
+
+// Assignment Code
+
+
+// Write password to the #password input
+function writePassword() {
+  
     // var password = generatePassword();
     // var passwordText = document.querySelector("#password");
 
@@ -26,4 +43,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
