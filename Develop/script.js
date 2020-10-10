afaecdd2a7e813af5fc8b24ee84be44212cc5c62
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-10 17:56:49 -0400

    added concat to arrays

--- Develop/script.js
+++ Develop/script.js
@@ -4,28 +4,58 @@ let numArr = [0,1,2,3,4,5,6,7,8,9]
 let lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Selector to write generated password to:
+let passwordText = document.querySelector("#password");
+
 // Button selection and click event listener:
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", userPrompts);
+let generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", createPassword);
 
 
-// Prompts for user choices:
-function userPrompts () {
+// Function to create the password:
+function createPassword () {
   let specCharConf = confirm("Include special characters? (Hit 'ok' to include, 'cancel' to exclude)")
   let numConf = confirm("Include numbers?")
-  let upperConf = confirm("Include lower case letters?")
-  let lowerConf = confirm("Include upper case letters?")
+  let lowerConf = confirm("Include lower case letters?")
+  let upperConf = confirm("Include upper case letters?")
     if (specCharConf === false && numConf === false && upperConf === false && lowerConf === false) {
       alert("Please choose at least one character type to include!")
       return;
     }
-  let charCount = prompt("How many characters? Must be between 8 and 128");
+  let charCount = parseInt(prompt("How many characters? Must be between 8 and 128"));
     if (charCount === null || charCount === "" || charCount < 8 || charCount > 128 || isNaN(charCount) === true) {
       alert("Please enter a valid length/ enter a number!");
       return;
     } 
+  
+  //Place holder array for character options based on input:
+  let chosenChar = []
+
+  //Conditionals for options to add different character types:
+  if (specCharConf === true) {
+    chosenChar = chosenChar.concat(specCharArr)
+  } 
+  if (numConf === true) {
+    chosenChar = chosenChar.concat(numArr)
+  } 
+  if (lowerConf === true) {
+    chosenChar = chosenChar.concat(lowerCaseArr)
+  } 
+  if (upperConf === true) {
+    chosenChar = chosenChar.concat(upperCaseArr)
+  } 
+  
+  
+  
+  
 }
 
+
+
+/* let userInputs = {
+    specialCharConf: specCharConf, numConf: numConf, upperConf: upperConf, lowerConf: lowerConf, charCount: charCount,
+  }
+  console.log(userInputs);*/
 
 
 // Assignment Code
@@ -35,7 +65,7 @@ function userPrompts () {
 function writePassword() {
   
     // var password = generatePassword();
-    // var passwordText = document.querySelector("#password");
+    // 
 
     // passwordText.value = password;
 
