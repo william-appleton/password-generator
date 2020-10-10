fed028917cb45753975e4e4790e9ecc469e8a8ca
Imprudent Friend <ImprudentFriend@gmail.com>
2020-10-10 16:43:35 -0400

    fixed alert message

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Character Arrays:
-let specCharArr = [ !, #, $, %, &, (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, {, |, }, ~];
+let specCharArr = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "{", "|", "}", "~"];
 let numArr = [0,1,2,3,4,5,6,7,8,9]
 let lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -15,8 +15,8 @@ function writePassword() {
   confirm("Include upper case letters?")
   confirm("Include lower case letters?")
   let charCount = prompt("How many characters? Must be between 8 and 128");
-    if (charCount === null || charCount === "" || charCount < 8 || charCount > 128) {
-      alert("Please enter a valid length!");
+    if (charCount === null || charCount === "" || charCount < 8 || charCount > 128 || isNaN(charCount) === true) {
+      alert("Please enter a valid length/ enter a number!");
     } 
     // var password = generatePassword();
     // var passwordText = document.querySelector("#password");
